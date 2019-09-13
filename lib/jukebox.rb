74ea39a921aa8fb169f9e9ec0cd1a1368bc2f4b8
Rei M <r.clarkemitchell@gmail.com>
2019-09-13 10:47:45 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,16 +1,16 @@
-require "pry"
 def help
-  command_hash = {
-	"help" => "displays this help message",
-	"list" => "displays a list of songs you can play",
-	"play" => "lets you choose a song to play",
-	"exit" => "exits this program"
-}
-puts "I accept the following commands:"
-command_hash.each do |commands, values|
-  puts "- #{commands} : #{values}"
-end
-end
+    command_hash = {
+	     "help" => "displays this help message",
+	      "list" => "displays a list of songs you can play",
+	       "play" => "lets you choose a song to play",
+	        "exit" => "exits this program"
+        }
+        puts "I accept the following commands:"
+        command_hash.each do |commands, values|
+          puts "- #{commands} : #{values}"
+        end
+      end
+
 
 def list(songslist)
   songslist.length.times {|n| puts "#{n + 1}. #{songslist[n]}"}
@@ -34,34 +34,19 @@ def exit_jukebox
   puts "Goodbye"
 end
 
-def prompt_user
-puts "Please enter a command:"
-
-end
-
-
-
 def run(arraylist)
-  prompt_user
-  user_input = gets.strip
-
   loop do
-
-  if user_input == 'exit'
-    exit_jukebox
-    break
-  elsif user_input == 'help'
-    help
-    prompt_user
+    puts "Please enter a command:"
     user_input = gets.strip
-  elsif user_input == 'play'
-    play(arraylist)
-    prompt_user
-    user_input = gets.strip
-  elsif user_input == 'list'
-    list(arraylist)
-    prompt_user
-    user_input = gets.strip
+    if user_input == 'exit'
+      exit_jukebox
+      break
+    elsif user_input == 'help'
+      help
+    elsif user_input == 'play'
+      play(arraylist)
+    elsif user_input == 'list'
+      list(arraylist)
+    end
   end
-end
 end
